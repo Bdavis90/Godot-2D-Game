8b4e8f4f1bb80f0ca24dd4b77ad021deef7ffea2
Brandon Davis <wowzerss90@yahoo.com>
2024-04-30 23:06:32 -0400

    Added custom inputs and added new sprite animations to player

--- Scripts/killzone.gd
+++ Scripts/killzone.gd
@@ -1,16 +1,10 @@
 extends Area2D
 
-@onready var camera = $"../Player/Camera2D"
+#@onready var camera = $"../Player/Camera2D"
 @onready var timer = $Timer
-var player: Player
 
 func _on_body_entered(body):
 	print("You died!")
-	#player = body as Player
-	#player.SPEED = 0
-	#player._launch_player()
-	#camera.limit_bottom = 0
-	#camera.limit_top = 0
 	Engine.time_scale = 0.5
 	body.get_node("CollisionShape2D").queue_free()
 	timer.start()
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,31 +1,49 @@
 extends CharacterBody2D
-class_name Player
 
-var SPEED = 130.0
+const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
+@onready var animated_sprite = $AnimatedSprite2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
-func _launch_player():
-	velocity.y += JUMP_VELOCITY
-
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		animated_sprite.play("jumping")
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("move_left", "move_right")
+	
+	#n Flip the Sprite
+	if direction < 0:
+		animated_sprite.flip_h = true
+	elif direction > 0:
+		animated_sprite.flip_h = false
+	
+	# Play animations
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("run")
+	else:
+		animated_sprite.play("jumping")
+		
+		
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
 	move_and_slide()
+
+
+
+func _launch_player():
+	velocity.y += JUMP_VELOCITY
